a00546e72ed58422d0125c5cfadfd5e9163fd0f7
Wu Niutian <wuniutian@gmail.com>
2022-11-07 22:01:23 +0800

    added the timer-

--- Testing Timer/ContentView.swift
+++ Testing Timer/ContentView.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var date = Date()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(timePeriodTest().day!) Days Left")
+                .font(.largeTitle)
+            
+            if #available(iOS 14.0, *) {
+                DatePicker("Choose the day", selection: $date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .frame(maxHeight: 400)
+            } else {
+                DatePicker("Choose the day", selection: $date)
+                    .frame(maxHeight: 400)
+            }
         }
-        .padding()
+        
+    }
+    
+    func timePeriodTest() -> DateComponents {
+        let userDate = Calendar.current.dateComponents([.day, .month, .year], from: date)
+        
+        let userDateComponents = DateComponents(calendar: Calendar.current, year: userDate.year!, month: userDate.month!, day: userDate.day!).date!
+        
+        let daysUntil = Calendar.current.dateComponents([.day], from: Date(), to: userDateComponents)
+        
+        return daysUntil
     }
 }
-
+  
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
