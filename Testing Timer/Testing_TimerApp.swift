52558e3acb5d4d01768f13c4468cffd385c69844
Wu Niutian <wuniutian@gmail.com>
2022-11-07 21:24:12 +0800

    Initial Commit

---
+++ Testing Timer/Testing_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_TimerApp.swift
+//  Testing Timer
+//
+//  Created by    wuniutian on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Testing_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
